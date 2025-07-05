db5f3d45015e36217144e95aa2183ffe1b17ca74
Weather-mv <mousam07999@gmail.com>
2025-07-05 15:52:41 +0530

    Movie Card

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import MovieCard from './components/MovieCard'
+
+const App = () => {
+  return (  
+
+    <>
+    <MovieCard movie={{title: "Weather Film", release_date: "2025"}}/>
+
+
+    </>
+  )
+}
+
+
+
+
+
+export default App;
